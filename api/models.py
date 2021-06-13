d7fe700c88fcdc25aaf841f2082b5c55f66c0081
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2021-06-13 11:37:30 +0300

    second commit

---
+++ api/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class MainCycle(models.Model):
+    user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
+    coins_count = models.IntegerField(default=0)
+    click_power = models.IntegerField(default=1)
+    auto_click_power = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
+
+    def click(self):
+        self.coins_count += self.click_power
+
+        if self.coins_count > self.is_level_up():
+            self.level += 1
+            return True
+        return False
+
+    def is_level_up(self):
+        # 0 > 1000; 1 > 2000; 2 > 5000 ...
+        return (self.level**2+1)*1000
+
+
+class Boost(models.Model):
+    main_cycle = models.ForeignKey(MainCycle, null=False, on_delete=models.CASCADE)
+    power = models.IntegerField(default=1)
+    price = models.IntegerField(default=20)
+    level = models.IntegerField(default=0)
+    boost_type = models.IntegerField(default=0)
+
+    def update_boost(self):
+        if self.main_cycle.coins_count < self.price:
+            return False
+        
+        self.main_cycle.coins_count -= self.price
+        self.level += 1
+        self.power = self.power + (self.power * 0.2)
+        if self.level > 25:
+            self.price *= 3
+        else:
+            self.price *= 1.1
+
+        if self.boost_type == 1:
+            self.main_cycle.auto_click_power += self.power
+            if self.level > 25:
+                self.price *= 5
+            else:
+                self.price *= 1.5
+        elif self.boost_type == 0:
+            self.main_cycle.click_power += self.power
+
+        self.main_cycle.save()
+
+        return self
+
